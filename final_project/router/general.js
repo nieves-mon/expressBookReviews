5cb5d9ea480313b9a3f018820c8f612067399867
nieves-mon <cunanan.nm@gmail.com>
2023-10-22 15:11:35 +0800

    implement get book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,7 +11,7 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (_req, res) {
-  return res.status(200).json(books);
+  return res.status(200).json(books.values);
 });
 
 // Get book details based on ISBN
@@ -65,8 +65,18 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+
+  if (!isbn.length) {
+    res.status(400).send({ message: "ISBN not specified" });
+  }
+
+  const book = books[isbn];
+  if (!book) {
+    res.status(404).send({ message: "Book not found" });
+  }
+
+  res.status(200).send(book.reviews);
 });
 
 module.exports.general = public_users;
