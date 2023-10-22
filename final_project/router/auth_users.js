ed4705d7a8b5d7f336bdb5558cf50b94bffa53f5
nieves-mon <cunanan.nm@gmail.com>
2023-10-22 16:09:01 +0800

    implement login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,50 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  //returns boolean
+  //write code to check is the username is valid
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  //returns boolean
+  //write code to check if username and password match the one we have in records.
+};
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username?.length || !password?.length) {
+    res
+      .status(400)
+      .send({ message: "Both username and password are required" });
+    return;
+  }
+
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (!user) {
+    res.status(401).send({ message: "Invalid username and/or password" });
+    return;
+  }
+
+  return res.status(200).send({
+    message: "User has successfully logged in",
+    token: jwt.sign({ user: user }, "access"),
+  });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
